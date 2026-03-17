edde2765561711040b4a3ed12b24930b89bdbba3
Jingnan Si <jingnan.si@gmail.com>
2026-03-16 20:47:59 -0700

    fix saml script

--- vpn_saml.sh
+++ vpn_saml.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
+set -x
+
 eval $(python3 saml.py)
 
 if test "x$INVALID_SAML_VALUES" = "xyes"; then
   exit 1
 fi
 
+env
+
 echo ${OC_COOKIE} | openconnect \
-	--protocol=gp \
-	--useragent=\'"${OC_USERAGENT}"\' \
-	--allow-insecure-crypto \
-	--user=${OC_USER} \
-	--os=${OC_OS} \
-	--usergroup=\'"${OC_USERGROUP}"\' \
-	--csd-user=nobody \
-	--csd-wrapper=/libexec/openconnect/hipreport.sh \
-	--authgroup=\'"${VPN_GW}"\' \
-	--passwd-on-stdin \
-	${VPN_PORTAL}
+		--protocol=gp \
+		--useragent=''"${OC_USERAGENT}"'' \
+		--user=${OC_USER} \
+		--os=${OC_OS} \
+		--usergroup=''"${OC_USERGROUP}"'' \
+		--csd-user=nobody \
+		--csd-wrapper=/libexec/openconnect/hipreport.sh \
+		--authgroup=''"${VPN_GW}"'' \
+		${VPN_PORTAL}
